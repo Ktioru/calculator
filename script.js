8702d10a9dda8fc96828314d375f4b888135a299
Ktioru <mateuscst45@gmail.com>
2023-01-05 12:28:37 -0300

    Added operate function to show the result on the HTML page

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let displayValue = []
 let operations = []
+
+let point = ""
 function add(...args) {
     let total = 0
     args.forEach((number) => {
@@ -32,6 +34,35 @@ function divide(firstNumber, ...args) {
     return total
 }
 
+function operate(numbers, operations) {
+    let total = 0
+    for (c = 0; c < numbers.length; c++) {
+        if (c == 0 && total == 0) {
+            total = numbers[c]
+        } else {
+            switch (operations[c - 1]) {
+                case "+":
+                    total = add(total, numbers[c])
+                    break;
+                case "-":
+                    total = subtract(total, numbers[c])
+                    break;
+                
+                case "x":
+                    total = multiply(total, numbers[c])
+                    break;
+                
+                case "÷":
+                    total = divide(total, numbers[c])
+                    break;
+            }
+            
+            
+        }
+        
+    }
+    return total
+}
 
 function startCalculator() {
     const input = document.querySelector("input")
@@ -39,17 +70,33 @@ function startCalculator() {
     console.log(buttons)
     buttons.forEach((number) => {
         number.addEventListener("click", () => {
-            input.value += number.textContent
+            if (number.textContent == "." && point == "" && input.value.includes(".") == false) {
+                point = "."
+            } else {
+                input.value += point + number.textContent
+                point = ""
+            }
+            
     })
 })
     const operationBtns = document.querySelectorAll(".operation")
     operationBtns.forEach((button) => {
         button.addEventListener("click", () => {
-            displayValue.push(input.value)
+            displayValue.push(Number(input.value))
             operations.push(button.textContent)
             input.value = ""
 
         })
+    })
+    const equalBtn = document.querySelector(".equal")
+    equalBtn.addEventListener("click", () => {
+        displayValue.push(Number(input.value))
+        console.log(displayValue)
+        result = operate(displayValue, operations)
+        input.value = result
+        total = 0
+        displayValue = []
+        operations = []
     })
 }
 
